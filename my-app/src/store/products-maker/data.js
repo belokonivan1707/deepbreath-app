a99f907ed966874cdb87fbe69303983357befccb
Ivan Bilokon <Belokonivan1707@gmail.com>
2021-06-26 18:55:01 +0300

    field the date product and the data products makers

--- my-app/src/store/products-maker/data.js
+++ my-app/src/store/products-maker/data.js
@@ -1,8 +1,8 @@
 const PRODUCTS_MAKERS = [
   {
     id: 1,
-    name: "Cris Feilung",
-    products: [1, 2],
+    name: "Cristy Stainford",
+    productsID: [1, 2, 3, 4, 5],
     description: "",
     businessTags: ["fuck people", "kill animals", "masturbation"],
     images: [],
@@ -10,9 +10,30 @@ const PRODUCTS_MAKERS = [
   },
   {
     id: 2,
-    name: "Curt Cobein",
-    products: [3],
+    name: " Nathaniel Merriweather",
+    productsID: [6, 7, 8, 9, 10],
+    description: "",
+    businessTags: ["fuck people", "kill animals", "masturbation"],
+    images: [],
     city: "Brooklyn",
+  },
+  {
+    id: 3,
+    name: "Chris Nowinski",
+    productsID: [11, 12, 13, 14, 15],
+    description: "",
+    businessTags: ["fuck people", "kill animals", "masturbation"],
+    images: [],
+    city: "New York",
+  },
+  {
+    id: 4,
+    name: "Erica Zoidberg",
+    productsID: [16, 17, 18, 19, 20],
+    description: "",
+    businessTags: ["fuck people", "kill animals", "masturbation"],
+    images: [],
+    city: "Sydney",
   },
 ];
 
