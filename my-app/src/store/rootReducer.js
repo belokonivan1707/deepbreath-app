21e3ff570a27557579fdfef3511bfa9492e7e493
Ivan Bilokon <Belokonivan1707@gmail.com>
2021-07-02 11:32:01 +0300

    create reducer for landings

--- my-app/src/store/rootReducer.js
+++ my-app/src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import headerReducer from "./header-menus/reducer";
 import productMakersReducer from "./products-maker/reducer";
 import productReducer from "./products/reducer";
+import landingsReducer from "./landings/reducer";
 
 const rootReducer = combineReducers({
   productStore: productReducer,
   productMakersStore: productMakersReducer,
   headerState: headerReducer,
+  landingsState: landingsReducer,
 });
 
 export default rootReducer;
