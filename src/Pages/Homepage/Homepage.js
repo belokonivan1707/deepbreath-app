bca657631c8a68a9244070ec3cc1861dd56881a2
Ivan Bilokon <Belokonivan1707@gmail.com>
2021-07-07 11:30:01 +0300

    test commit after added prettier configurate

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,9 +1,9 @@
-import Header from "../../containers/header-container/header-container";
-import HeaderFilter from "../../containers/header-filter-container/header-filter-container";
-import ProductsContainer from "../../containers/products-container/products-container";
-import Dropdown from "../../components/dropdown/dropdown";
-import { Footer } from "../../components/footer/footer";
-import { Wrap } from "./styling";
+import Header from '../../containers/header-container/header-container'
+import HeaderFilter from '../../containers/header-filter-container/header-filter-container'
+import ProductsContainer from '../../containers/products-container/products-container'
+import Dropdown from '../../components/dropdown/dropdown'
+import { Footer } from '../../components/footer/footer'
+import { Wrap } from './styling'
 
 const Homepage = () => {
   return (
@@ -16,7 +16,7 @@ const Homepage = () => {
       </Wrap>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Homepage;
+export default Homepage
